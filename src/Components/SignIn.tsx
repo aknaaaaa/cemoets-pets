5d3d16a19413146b401f3e5f53255fdce2a2a538
aknaaaaa <zent150708@gmail.com>
2025-04-24 15:31:32 +0700

    Add files via upload

--- src/Components/SignIn.tsx
+++ src/Components/SignIn.tsx
@@ -22,12 +22,12 @@ function SignIn() {
               className="placeholder:text-lg w-full border-b border-gray-400 bg-transparent focus:outline-none focus:border-blue-500 transition duration-300 py-2"
             />
           </div>
-          <button className="px-10 py-2  text-2xl rounded-md border-2 bg-sky-600 hover:bg-slate-100 hover:border-sky-600">
+          <button className="px-10 py-2  text-2xl rounded-md border-2 bg-sky-600 hover:bg-slate-100 hover:border-sky-600 transition duration-300 ease-in-out">
             Login
           </button>
           <p className="font-semibold ">
             Don't have an account?{" "}
-            <a href="#" className="text-sky-600 hover:underline">
+            <a href="SignUp.tsx" className="text-sky-600 hover:underline ">
               Register Now!
             </a>
           </p>
--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -27,7 +27,7 @@ function SignUp() {
               className="placeholder:text-lg  w-full border-b border-gray-400 bg-transparent focus:outline-none focus:border-blue-500 transition duration-300 py-2"
             />
           </div>
-          <button className="px-10 py-2  text-2xl rounded-md border-2 bg-sky-600 hover:bg-slate-100 hover:border-sky-600">
+          <button className="px-10 py-2  text-2xl rounded-md border-2 bg-sky-600 hover:bg-slate-100 hover:border-sky-600 transition duration-300 ease-in-out">
             Register
           </button>
         </div>
